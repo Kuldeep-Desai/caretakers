5e05eee68087f7933ecb17c5f7facdf194046ef0
Admin <admin@Admins-MacBook-Pro.local>
2016-01-04 15:55:39 +0530

    added routing

--- www/js/app.js
+++ www/js/app.js
@@ -3,23 +3,87 @@
 // angular.module is a global place for creating, registering and retrieving Angular modules
 // 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
 // the 2nd parameter is an array of 'requires'
-angular.module('starter', ['ionic','firebase'])
-.factory('CareTakers',['$firebaseArray',function($firebaseArray){
-    var itemsRef=new Firebase('https://caretakers.firebaseio.com/careTaker');
+angular.module('starter', ['ionic', 'firebase'])
+  .factory('Staff', ['$firebaseArray', function ($firebaseArray, $firebase) {
+    var url = 'https://caretakers.firebaseio.com/staff';
+    var itemsRef = new Firebase(url);
     return $firebaseArray(itemsRef);
-}]).
-controller('ListCtrl',function($scope, $ionicListDelegate, CareTakers){
-    $scope.careTakers=CareTakers;
-    
-    $scope.addCareTaker=function(){
-        var name=prompt('Enter Name of Care Taker : ');
-        if(name){
-            $scope.careTakers.$add({
-                'name':name
-            });
+  }])
+  .factory('Auth', function ($firebaseAuth) {
+    var url = 'https://caretakers.firebaseio.com/users';
+    var itemsRef = new Firebase(url);
+    return $firebaseAuth(itemsRef);
+  })
+  
+  .controller("loginCtrl", function ($scope, Auth) {
+    //$scope.authData = null;
+    $scope.login = function () {
+      Auth.$authWithOAuthRedirect("facebook").then(function (authData) {
+        console.log(authData);
+        $scope.authData = authData;
+      }).catch(function (error) {
+        if (error.code === "TRANSPORT_UNAVAILABLE") {
+          Auth.$authWithOAuthPopup("facebook").then(function (authData) {
+            console.log("From Popup", authData);
+            $scope.authData = authData;
+          });
         }
-    }
-});
+        else {
+          console.log(error);
+        }
+      });
+    };
+    $scope.logout = function () {
+      Auth.$unauth();
+    };
+    Auth.$onAuth(function (authData) {
+      if (authData === null)
+        console.log("Not logged in yet");
+      else
+        console.log("Logged in as : ", authData.uid);
+      $scope.authData = authData;
+    });
+  })
+  .controller("homeCtrl", function ($scope) {
+
+  })
+  .config(function ($stateProvider, $urlRouterProvider) {
+    $stateProvider
+      .state("login", {
+        url: "/login",
+        templateUrl: "templates/login.html",
+        controller: "loginCtrl"
+      })
+      .state("home", {
+        url: "/home",
+        templateUrl: "templates/home.html",
+        controller: "homeCtrl"
+      })
+      .state("staff", {
+        url: "/staff",
+        templateUrl: "templates/staff.html",
+        controller: "ListCtrl",
+        resolve: {
+          $currentAuth : function (Auth) {
+            return Auth.$requireAuth();
+          }
+        }
+      });
+    $urlRouterProvider.otherwise("/login");
+
+  })
+  .controller('ListCtrl', function ($scope, $ionicListDelegate, Staff) {
+    $scope.careTakers = Staff;
+
+    $scope.addStaff = function () {
+      var name = prompt('Enter Name of Care Taker : ');
+      if (name) {
+        $scope.careTakers.$add({
+          "name": name
+        });
+      }
+    };
+  })
 /*.run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {
@@ -37,3 +101,12 @@ controller('ListCtrl',function($scope, $ionicListDelegate, CareTakers){
     }
   });
 })*/
+.run(["$rootScope", "$state", function ($rootScope, $state) {
+  $rootScope.$on("$stateChangeError", function (event, toState, toParams, fromState, fromParams, error) {
+    if (error === "AUTH_REQUIRED") {
+      $state.go("login");
+    }
+  });
+ 
+}]);
+
